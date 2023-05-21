5c17ad0d4e71fd8961fad814daedb1a65fba43ab
teapiall <shuaizheng06@126.com>
2023-05-21 12:31:52 +0800

    update

--- dockerCp.sh
+++ dockerCp.sh
@@ -3,11 +3,8 @@
 for j in {1..5}
 do
 
-docker cp ~/teapiall/2023huayi/2023learning/html/cme.html chrome$j:/home/chrome/
 docker cp ~/teapiall/2023huayi/2023learning/exam.js/exam230412.js chrome$j:/home/chrome/
 docker cp ~/teapiall/2023huayi/2023learning/exam.js/onlyplay.js chrome$j:/home/chrome/
 
-
 done
-
 echo "复制完毕！！！"
