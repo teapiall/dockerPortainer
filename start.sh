334d6e725af26d8bafb0c37f5af624738041ef91
teapiall <shuaizheng06@126.com>
2023-06-07 20:27:37 +0800

    update

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 for j in {2..6};do
 
-docker start hrome$j-172.117.0.$j
+docker start chrome$j-172.117.0.$j
 echo "chrome$j-172.117.0.$j start success"
 done
 echo "Thinks you use"
--- stop.sh
+++ stop.sh
@@ -2,7 +2,7 @@
 
 for j in {2..6};do
 
-docker stop hrome$j-172.117.0.$j
+docker stop chrome$j-172.117.0.$j
 
 echo "chrome$j-172.117.0.$j stop success"
 
