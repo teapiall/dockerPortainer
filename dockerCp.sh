7a3a6afa025fc36e1943aba89ecadaab669c86f9
teapiall <shuaizheng06@126.com>
2023-06-07 19:33:51 +0800

    update

--- dockerCp.sh
+++ dockerCp.sh
@@ -3,8 +3,8 @@
 for j in {1..5}
 do
 
-docker cp ~/teapiall/2023huayi/2023learning/exam.js/exam230526.js chrome$j:/home/chrome/
+docker cp ~/teapiall/2023huayi/2023learning/exam.js/91huayi_exam.js chrome$j:/home/chrome/
 docker cp ~/teapiall/2023huayi/2023learning/exam.js/onlyplay.js chrome$j:/home/chrome/
-
+echo "chrome$j  复制完成"
 done
-echo "复制完毕！！！"
+echo "全部复制完毕！！！"
--- dockercreate.sh
+++ dockercreate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-for j in {1..5}
+docker network create --subnet=172.117.0.0/16 chromenet
+for j in {2..6}
 do
-docker run -dit --name chrome$j -p 590$j:5900  siomiz/chrome
+docker run -dit --name chrome$j-172.117.0.$j --net chromenet --ip 172.117.0.$j -p 590$j:5900  siomiz/chrome
 
 done
  
